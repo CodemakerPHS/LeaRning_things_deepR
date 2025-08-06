498e7515ec8d373207387fc8be7d8f0d14ac023f
CodemakerPHS <pauline.ward5@phs.scot>
2025-08-06 09:32:31 +0000

    embracing hurrah

--- data_masking.R
+++ data_masking.R
@@ -20,3 +20,30 @@ stranded_data |>
   select(age) |>
   head(7)
 
+stranded_data |>
+  select("age") |>
+  head(7)
+
+# suppoly quoted col name and get back a 1-col tibble
+# Base R??
+masc_fun <- function(the_df, the_col_name){
+  the_result <- the_df[the_col_name]
+  return(the_result)
+}
+
+masc_fun(stranded_data, "age")
+
+tidy_mask_fun <- function(the_col_name){
+  the_tidy_result <- stranded_data[the_col_name]
+  return(the_tidy_result)
+}
+tidy_mask_fun("age")
+tidy_mask_fun(age)
+
+tidy_embraced_mask_fun <- function(the_col_name){
+  the_tidy_result <- stranded_data |>
+    select({{the_col_name}})
+  return(the_tidy_result)
+}
+#tidy_embraced_mask_fun("age")
+tidy_embraced_mask_fun(age)
