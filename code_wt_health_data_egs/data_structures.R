296b3f4a6ee99db51f7f60d2db2508d01ab82db5
PaulineDataWard <polo@paulineward.me.uk>
2025-08-16 22:37:33 +0100

    the messageis loud n clear

--- code_wt_health_data_egs/data_structures.R
+++ code_wt_health_data_egs/data_structures.R
@@ -1,10 +1,14 @@
 ############
-# 
+#
 # Pauline July 2025
 # Cheat Sheet for R data structures
-# 
-# Before going further, need to wrangle github personal access token apparently 
-# https://docs.posit.co/cloud/guide/advanced/index.html#working-with-github 
+#
+# Before going further,
+# using positron handy auth integrn for github so far so good.
+# Else if (using posit cloud){
+#  might need to wrangle github personal access token apparently
+# https://docs.posit.co/cloud/guide/advanced/index.html#working-with-github
+# }
 # ###########
 
 library(here)
@@ -14,11 +18,13 @@ library(tidyverse)
 # Data dir is ../data
 data_dir_path <- "data"
 
-# Everything is an object. 
+# Everything is an object.
 
-# Example data - Quality Performance Indicators 
-# Provided by PHS under the Open Govt Licence 
+# Example data - Quality Performance Indicators
+# Provided by PHS under the Open Govt Licence
 # Warning: read_csv is just reading in all cols guessed as character type
-# when in fact some are numeric and some should be factors. 
-testis_QPIs_2020_2023 <- read_csv(here(data_dir_path, "2024_12_10_testis_qpi_summary_table.csv"))
-
+# when in fact some are numeric and some should be factors.
+testis_QPIs_2020_2023 <- read_csv(here(
+  data_dir_path,
+  "2024_12_10_testis_qpi_summary_table.csv"
+))
