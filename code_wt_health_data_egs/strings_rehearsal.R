f760cf0fe1f5a2e804390568216d1dab03e4e49a
PaulineDataWard <polo@paulineward.me.uk>
2025-08-23 22:21:37 +0100

    Working - can use lookarounds for preceding and following.

--- code_wt_health_data_egs/strings_rehearsal.R
+++ code_wt_health_data_egs/strings_rehearsal.R
@@ -5,7 +5,7 @@
 
 library("stringr")
 
-# Good example strings
+# Good example strings initialisation ----
 qpi_name <- "QPI 8(iii): Multi-disciplinary, special, good meeting"
 simple_qpi_name <- "QPI 12: Mindfulness available for 30 days"
 qpi_yr <- "2021/2022"
@@ -17,7 +17,7 @@ wee_str_vec <- c(
     'measurability',
     "Regional networks: mapping"
 )
-hosp_codes <- c("J103H", "J214H", "P101H", "J103H")
+hosp_codes <- c("J103H", "J214H", "X9999", "P101H", "J103H", "X1010")
 hb_names <- c(
     "Inverpo",
     "Strathseine and Lammermuir",
@@ -26,6 +26,7 @@ hb_names <- c(
 sites <- c("C67.1", "C67.5", "C67.8")
 site_names <- c("feather", "paw", "noggin")
 
+# Detect presence, return logical Boolean
 str_detect(qpi_name, "QPI 2") # FALSE
 str_detect(qpi_name, "QPI 8") # TRUE
 
@@ -66,7 +67,7 @@ str_extract(myfilenm, "validations-(.+)\\.xlsx") # "validations-v4-0.xlsx"
 str_extract(myfilenm, "validations-(.*?)\\.xlsx") # "validations-v4-0.xlsx"
 
 # The key is to use str_match instead of str_extract AND THEN get second elt of matrix.
-# Because str_extract returns the first match (is vectorise).
+# Because str_extract returns the first match (is vectorised).
 # Whereas str_match returns a matrix with a column for each () group.
 mymatches <- str_match(myfilenm, "validations-(.*?)\\.xlsx")
 mymatches[, 2]
@@ -74,11 +75,11 @@ mymatches <- str_match(myfilenm, "validations-(.*?)\\.xlsx?")
 mymatches[, 2]
 myfilenm <- "brain-and-cns-cancer-qpi-validations-v4-0.xls"
 
-# try lookarounds
-# a preceded by r     (?<=r)a
-# a not preceded by r (?<!r)a
-# a followed by m     a(?=m)
-# a not followed by m a(?!m)
+# Lookarounds
+# a preceded by r       (?<=r)a
+# a not preceded by r   (?<!r)a
+# a followed by m      a(?=m)
+# a not followed by m  a(?!m)
 raw_input <- "dramatic"
 str_extract(raw_input, "(?<=r)a") # a preceded by r
 str_extract(raw_input, "(?<=r)a.*") # a preceded by r, and everything after the a.
@@ -86,8 +87,26 @@ str_extract(raw_input, "(?<!r)a.*") # a not preceded by r and then everything.
 str_extract(raw_input, "a(?=m).*") # a followed by m, and then everything after the m.
 str_extract(raw_input, "a(?!m).*") # a not followed by m and then everything.
 
+## Use lookarounds to get the version number out of middle of filename
+# (?<) or (?), with = or !, to extract string preceded by or followed by the matches.
+str_extract(myfilenm, "(?<=-).*") # everything after the 1st dash
+str_extract(myfilenm, "(?<=validations).*") # evthing after validations
+str_extract(myfilenm, ".*(.x)") # wrong, did not escape dot, just gets everything
+# No question mark, no lookaround, just treats brackets as a group.
+str_extract(myfilenm, ".*(\\.x)") # use \\ to escape . to isolate filename extension
+str_extract(myfilenm, "(?<=validations-).*(?=.xls)") # Success: "v4-0"
 
+## Use integer indices ##
 str_sub(hosp_codes, start = 1L, end = 1L) # first character, indicating geographical area
 str_sub(hosp_codes, start = 2L, end = 4L) # second to fourth chars, identifying hospital
 
 str_replace_all(hb_names, "&", "and")
+
+## Pitfalls ##
+# Don't bother with str_like(), it just allows you to use same wildcards etc as SQL LIKE.
+
+# Beware str_start(), won't work correctly with a pipe for alternate patterns.
+# Better stick with the anchors ^ and $.
+# To vectorise...?
+str_start(hosp_codes, "J|P")
+str_detect(hosp_codes, "^J|^P")
